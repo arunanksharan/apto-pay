2f61adf2149056d926ec3df49bbe848f18e9bbb2
arunanksharan <arunank.sharan@gmail.com>
2024-03-26 13:55:30 +0530

    bug: modify APT decimals to 8

--- core/src/constants.ts
+++ core/src/constants.ts
@@ -10,7 +10,4 @@ export const HTTPS_PROTOCOL = 'https:';
 export const COIN_ADDRESS = '0x1::coin';
 
 /** @internal */
-export const APT_DECIMALS = 6; // ToDo: Check and confirm
-
-/** @internal */
-export const TEN = new BigNumber(10); // ToDo: Check and confirm
+export const APT_DECIMALS = 8;
